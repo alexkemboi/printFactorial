fed6d63d3a515473bc64b863de0dccf93bd6b7b4
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-19 14:56:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static int printFactorial(int term)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= term; i++)
+        {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number to find factorial");
+        int term = 0;
+        term=Convert.ToInt32(Console.ReadLine());
+        int solution=printFactorial(term);
+        Console.WriteLine($"The factorial of {term} is:{solution}");
+
+    }
+}
